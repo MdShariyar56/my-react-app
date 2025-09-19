005a3f5b9e903d7d1f34ddbf2a42c36dcfb7402c
MdShariyar56 <shahriyarkobir2005@gmail.com>
2025-09-19 11:21:27 +0600

    Added a card visited button and card color

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 
 const Country = ({ country }) => {
-    console.log(country.population.population)
+    const [visited, setVisited] = useState(false)
+   // console.log(country.area.area)
+const handleVisit = () =>{
+    setVisited(true)
+    //console.log('buttn clicked')
+    setVisited(!visited)
+}
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>Name: {country.name.common}</h3>
             <h4>Population: {country.population.population}</h4>
+            <p>Area: {country.area.area} {country.area.area > 30000 ? "Big Country" : "Small country"}</p>
+            <button onClick={handleVisit}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
         </div>
     );
 };
